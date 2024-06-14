e4aba5d95677808eff586448d7349febc18bd779
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-06-14 22:42:44 +0200

    Added zsh-vi-mode

--- home/common/default.nix
+++ home/common/default.nix
@@ -5,6 +5,7 @@
 
 	programs.starship = {
 		enable = true;
+		enableZshIntegration = true;
 	};
 
 	home.stateVersion = "24.05";
@@ -14,4 +15,25 @@
 	home.packages = with pkgs; [
 		cowsay
 	];
+
+	programs.zsh = {
+		enable = true;
+		shellAliases = {
+			ll = "ls -la";
+		};
+
+		plugins = [
+			{
+				# https://discourse.nixos.org/t/zsh-users-how-do-you-manage-plugins/9199/8
+				name = "zsh-vi-mode";
+				src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
+			}
+		];
+	};
+
+	programs.git = {
+		enable = true;
+		userName = "Jonathan Niklasson Godar";
+		userEmail = "jonathan.godar@hotmail.com";
+	};
 }

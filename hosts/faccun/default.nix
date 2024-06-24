5f92e80ea145aa83a6b07ee4cbef831a60200c61
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-06-24 15:02:25 +0200

    Remove graphics card support

--- hosts/faccun/default.nix
+++ hosts/faccun/default.nix
@@ -172,35 +172,6 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-
-    hardware.graphics = {
-      enable = true;
-    };
-
-
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-  };
-
-
-
-  
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];

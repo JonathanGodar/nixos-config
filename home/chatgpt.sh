995ea5f06857a269e1b85dd5895879da9df0b461
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-07-01 01:20:17 +0200

    Remove unnecessary call in chatgpt.sh

--- home/chatgpt.sh
+++ home/chatgpt.sh
@@ -1,6 +1,5 @@
 hyprctl dispatch togglespecialworkspace Chat 
 
-pgrep -f 'asdlfkjasdfl'
 active=$(pgrep -f -- '--app=https://chatgpt.com')
 if [ -z "$active" ]; then
   chromium --app="https://chatgpt.com" &

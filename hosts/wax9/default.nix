b62870f06c26b971a0e8a82bdd6db15807ce1e59
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-08-27 14:38:18 +0200

    Switch wax9 to the module branch

--- hosts/wax9/default.nix
+++ hosts/wax9/default.nix
@@ -1,9 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../common
   ];
   networking.hostName = "wax9";
+
   services.libinput.touchpad.naturalScrolling = true;
+  services.displayManager.sddm.package = pkgs.kdePackages.sddm;
 }

ea4d081549bbe0b09e425da26829442d294510fc
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-10-01 08:14:05 +0200

    Screen mirroring to 1920x1080

--- home/wax9/default.nix
+++ home/wax9/default.nix
@@ -9,4 +9,16 @@
     brightnessctl
     discord
   ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1, 1920x1080@60, 0x0, 1"
+      ", preferred, auto, 1, mirror, eDP-1"
+      # "DP-2, 1920x1080@144, 1920x0, 1"
+      # "Unknown-1, disable"
+    ];
+
+    "$mod" = "SUPER";
+    "$MAIN_MONITOR" = "DP-2";
+    "$OTHER_MONITOR" = "DP-1";
+    };
 }

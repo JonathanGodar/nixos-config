6f05b9d449d093973ba9521cdce0efd7f7c31977
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-12-17 01:13:44 +0100

    Add fancy index to faccun nginx

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
         hostname = "faccun";
         extraModules = [
           inputs.rnote-export.nixosModules.${system}.default
-          {
+          ({pkgs, ...}: {
             services.rnote-export = {
               enable = true;
               inputDirectory = "/home/jonathan/kth";
@@ -110,7 +110,22 @@
               group = "users";
               includeString = "*/föreläsningar/*.rnote";
             };
-          }
+
+            services.nginx = {
+              enable = true;
+              additionalModules = [ pkgs.nginxModules.fancyindex ];
+
+
+              virtualHosts."192.168.1.83" = {
+                root = "/var/lib/rnote-export/";
+                locations."/" = {
+                  extraConfig = ''
+                  fancyindex on;
+                  '';
+                };
+              };
+            };
+          })
         ];
       };
       wax9 = mkSystem rec {

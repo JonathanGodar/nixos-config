a8755b547640e3ceb4120c2992a948afce4593d9
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-07-26 22:42:35 +0200

    Remove old rnote export code

--- flake.nix
+++ flake.nix
@@ -110,33 +110,6 @@
           system = "x86_64-linux";
           hostname = "faccun";
           extraModules = [
-            inputs.rnote-export.nixosModules.${system}.default
-            (
-              { pkgs, ... }:
-              {
-                services.rnote-export = {
-                  enable = true;
-                  inputDirectory = "/home/jonathan/kth";
-                  user = "jonathan";
-                  group = "users";
-                  includeString = "*/föreläsningar/*.rnote";
-                };
-
-                services.nginx = {
-                  enable = true;
-                  additionalModules = [ pkgs.nginxModules.fancyindex ];
-
-                  virtualHosts."192.168.1.83" = {
-                    root = "/var/lib/rnote-export/";
-                    locations."/" = {
-                      extraConfig = ''
-                        fancyindex on;
-                      '';
-                    };
-                  };
-                };
-              }
-            )
           ];
         };
         wax9 = mkSystem rec {

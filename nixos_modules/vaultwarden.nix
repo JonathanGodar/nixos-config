887bff3a719385f510bd0b6934089e9704214a03
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-07-31 23:30:48 +0200

    Make vaultwarden use postgres

--- nixos_modules/vaultwarden.nix
+++ nixos_modules/vaultwarden.nix
@@ -9,12 +9,28 @@
     preconf.vaultwarden.enable = lib.mkEnableOption "Enable preconfigured vaultwarden";
   };
   config = lib.mkIf config.preconf.vaultwarden.enable {
-    services.vaultwarden = {
-      enable = true;
-      config = {
-        ROCKET_ADDRESS = "127.0.0.1";
-        ROCKET_PORT = 8222;
-        DOMAIN = "https://pass.ngodag.com";
+    services = {
+      vaultwarden = {
+        enable = true;
+        dbBackend = "postgresql";
+        config = {
+          DATABASE_URL = "postgresql:///vaultwarden?host=/run/postgresql";
+          ROCKET_ADDRESS = "127.0.0.1";
+          ROCKET_PORT = 8222;
+          DOMAIN = "https://pass.ngodag.com";
+        };
+      };
+
+      # Postgresql setup taken from atuin nixos service: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/misc/atuin.nix
+      # sudo -u vaultwarden psql to access the vaultwarden database via psql cli.
+      postgresql = {
+        ensureUsers = [
+          {
+            name = "vaultwarden";
+            ensureDBOwnership = true;
+          }
+        ];
+        ensureDatabases = [ "vaultwarden" ];
       };
     };
   };

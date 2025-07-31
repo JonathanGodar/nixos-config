3b36a517be95774fec455b40b870d7735d152717
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-07-31 23:29:47 +0200

    Re-remove zfs support

--- hosts/faccun/default.nix
+++ hosts/faccun/default.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+# Edit this configuration file to define hat should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
@@ -136,6 +136,10 @@
     wezterm
   ];
 
+  # boot.zfs.enabled = true;
+  # boot.supportedFilesystems = [ "zfs" ];
+  # services.zfs.enable = true;
+
   # services.ddclient = {
   #   enable = true;
   #   protocol = "namecheap";
@@ -239,7 +243,6 @@
 
     # Let NixOS install and configure the database automatically.
     database.createLocally = true;
-
     # Let NixOS install and configure Redis caching automatically.
     configureRedis = true;
 

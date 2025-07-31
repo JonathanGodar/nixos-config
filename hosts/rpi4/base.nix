cd4e12ab96cd4b1501cca2b9381dad9ff5091abc
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-07-31 23:30:16 +0200

    Format rpi4 base + add /mnt/ssd as tmpfile

--- hosts/rpi4/base.nix
+++ hosts/rpi4/base.nix
@@ -14,34 +14,40 @@
   # options.preconf.hyprland_de.enable = lib.mkEnableOption "Enable preconfigured hyprland with DE-like preconfigurations";
 
   options = {
-    rpi4_fs.enable = lib.mkOption {default = true; description = "Configure the raspberry pi 4 file system";};
+    rpi4_fs.enable = lib.mkOption {
+      default = true;
+      description = "Configure the raspberry pi 4 file system";
+    };
   };
 
   config = lib.mkIf config.rpi4_fs.enable {
     # This is set by the SD-installer and needs to be kept.
-    fileSystems."/" =
-    { 
+    fileSystems."/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
 
-    fileSystems."/mnt/ssd" =
-    { 
+    fileSystems."/mnt/ssd" = {
       device = "/dev/vg-ssd/lv-home";
       fsType = "ext4";
     };
 
-    boot.kernel.sysctl = {
-      "fs.inotify.max_user_watches"   = "524288";  # Increase default
+    systemd.tmpfiles.settings.rpi4-fs."/mnt/ssd".d = {
+      # group = cfg.group;
+      # user = cfg.user;
+      mode = "0755";
     };
 
-	#    boot = {
-	#      kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-	# loader = {
-	#   generic-extlinux-compatible.enable = lib.mkDefault true;
-	#   grub.enable = lib.mkDefault false;
-	# };
-	#    };
+    boot.kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "524288"; # Increase default
+    };
+
+    #    boot = {
+    #      kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    # loader = {
+    #   generic-extlinux-compatible.enable = lib.mkDefault true;
+    #   grub.enable = lib.mkDefault false;
+    # };
+    #    };
   };
 }
-

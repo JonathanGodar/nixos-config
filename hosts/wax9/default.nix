d7a7de368f5f574d1b2ff74a1dfd86a0b244060e
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-10-20 09:14:48 +0200

    Enable virtual box

--- hosts/wax9/default.nix
+++ hosts/wax9/default.nix
@@ -13,4 +13,6 @@
   services.libinput.touchpad.naturalScrolling = true;
   services.desktopManager.cosmic.enable = true;
   services.displayManager.sddm.package = pkgs.kdePackages.sddm;
+
+  preconf.virtualbox.enable = true;
 }
--- nixos_modules/default.nix
+++ nixos_modules/default.nix
@@ -4,5 +4,6 @@
     ./backup.nix
     ./vaultwarden.nix
     ./ntfy.nix
+    ./virtualbox.nix
   ];
 }

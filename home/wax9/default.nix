6fa5d6c85bcfa2a51253cbf64b8aed85120926c3
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2026-03-23 17:41:59 +0100

    Update inputs + Add vicinae

--- home/wax9/default.nix
+++ home/wax9/default.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  inputs,
+  # system,
+  ...
+}:
 {
   imports = [ ../common ];
 
@@ -7,6 +13,40 @@
   preconf.matlab.enable = true;
 
   preconf.hyprland.extraMediaKeyKeybinds = true;
+
+  programs.vicinae = {
+    enable = true;
+    systemd.enable = true;
+
+    extensions = with inputs.vicinae-extensions.packages.${pkgs.system}; [
+      nix
+      it-tools
+      bluetooth
+      wifi-commander
+      process-manager
+      player-pilot
+      pulseaudio
+      power-profile
+    ];
+  };
+  #   extensions = [
+  # (config.lib.vicinae.mkExtension {
+  # name = "test-extension";
+  # src =
+  # pkgs.fetchFromGitHub {
+  # owner = "schromp";
+  # repo = "vicinae-extensions";
+  # rev = "f8be5c89393a336f773d679d22faf82d59631991";
+  # sha256 = "sha256-zk7WIJ19ITzRFnqGSMtX35SgPGq0Z+M+f7hJRbyQugw=";
+  # }
+  # + "/test-extension";
+  # }
+  # (config.lib.vicinae.mkRayCastExtension {
+  # name = "gif-search";
+  # sha256 = "sha256-G7il8T1L+P/2mXWJsb68n4BCbVKcrrtK8GnBNxzt73Q=";
+  # rev = "4d417c2dfd86a5b2bea202d4a7b48d8eb3dbaeb1";
+  # }
+  # ]};
 
   home.packages = with pkgs; [
     brightnessctl

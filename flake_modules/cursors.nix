346f9569223282fa29f2f2adf6a39cdf6bcec55a
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2026-07-22 14:15:00 +0200

    Remove commnet

--- flake_modules/cursors.nix
+++ flake_modules/cursors.nix
@@ -2,8 +2,6 @@
   flake.modules.homeManager.cursors =
     { pkgs, ... }:
     {
-      # hyprcursor.programs.enable = true;
-
       home.pointerCursor = {
         enable = true;
         name = "phinger-cursors-dark";

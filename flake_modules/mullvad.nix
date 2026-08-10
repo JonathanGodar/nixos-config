1da885227d13b0ca53b036aafb2b218b3e7fef8d
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2026-08-10 13:37:27 +0200

    Add mullvad gui

--- flake_modules/mullvad.nix
+++ flake_modules/mullvad.nix
@@ -1,5 +1,8 @@
 {
-  flake.modules.nixos.mullvad = {
+  flake.modules.nixos.mullvad = { pkgs, ... }: {
+    environment.systemPackages = [
+      pkgs.mullvad-vpn # Make the mullvad gui available
+    ];
     services.mullvad-vpn.enable = true;
   };
 }

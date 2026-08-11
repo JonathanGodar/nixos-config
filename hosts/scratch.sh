e4fbbdeb7d6d897d0daf44a5c78d77f886d44982
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2026-08-11 16:22:20 +0200

    Remove legacy code

--- hosts/scratch.sh
+++
@@ -1,5 +0,0 @@
-if [[ $exitStatus -eq 0 ]]; then
-  notify-send -t 0 "Backup done"
-else
-  notify-send -u critical -t 0 "Backup failed" "Backup job exited with status $exitStatus"
-fi
--- nixos_modules/backup.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, lib, config, ...}: {
-  options.preconf.backups = {
-    enable = lib.mkEnableOption "Enable backups";
-  };
-
-  config = lib.mkIf config.preconf.backups.enable {
-    # boot.zfs.enabled = true;
-  };
-}
